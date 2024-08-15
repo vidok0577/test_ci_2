13b27e23549d11df2a654caf562d3ec8634965f2
Igor Vazhenin <vidok0577@gmail.com>
2024-08-15 18:13:41 +0500

    change

--- src/js/unit.js
+++ src/js/unit.js
@@ -1,7 +1,5 @@
 export function sortUnitLevelHealth(listUnit) {
-    listUnit.sort((item1, item2) => {
+    return listUnit.sort((item1, item2) => {
         return (item1.health >= item2.health) ? -1 : 1;
     });
-    console.log(listUnit);
-    return listUnit;
 }
